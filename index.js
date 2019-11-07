26423e310fd653a3763aeda276303815d2ea13ac
Adena McGibbon <justadena@gmail.com>
2019-11-06 18:16:56 -0700

    Added a bit of Pseudo code

---
+++ index.js
@@ -0,0 +1,9 @@
+//Pull it together app
+
+//A command line app that generates a PDF populated with the developer's profile image
+// GitHub username
+//links to: user location via google maps, user github profile, user blog
+//bio
+//# of public repositories
+//# of github stars
+//# of users following
