f436edbf3ddcaecbe9ad689fd620f864dda5e308
Adena McGibbon <justadena@gmail.com>
2019-11-08 20:40:30 -0700

    Updated variables with required info from API call

--- index.js
+++ index.js
@@ -1,44 +1,3 @@
-//Pull It Together App
-
-//A command line app that generates a PDF populated with 
-//the developer's profile image
-// GitHub username
-//links to: user location via google maps, user github profile, user blog
-//bio
-//# of public repositories
-//# of github stars
-//# of users following
-
-//GitHub API
-
-// the baground color for the pdf should match the user specified color
-
-//create inquirer question to get username, favorite color, 
-//run the api call
-//when I get the response save the required info to variables
-//
-//write those variables to a pdf file with the favorite color as the background
-// Functional, deployed application.
-
-
-// GitHub repository with a unique name and a README describing project.
-
-
-// The application generates a PDF resume from the user provided GitHub profile.
-
-
-// The generated resume includes a bio image from the user's GitHub profile.
-
-
-// The generated resume includes the user's location and a link to their GitHub profile.
-
-
-// The generated resume includes the number of: public repositories, followers, GitHub stars and following count.
-
-
-// The background color of the generated PDF matches the color that the user provides.
-// https://api.github.com/users/AdenasGittinIt/repos - loop through this api and get the sumation of all stargazer counts
-
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
@@ -68,10 +27,22 @@ let following = res.data.following;
 let publicRepos = res.data.public_repos;
 let location = res.data.location;
 let profilePic = res.data.avatar_url;
+let profileUrl = res.data.html_url;
+let blogUrl = res.data.blog;
+let bio = res.data.bio;
+let name = res.data.name;
 
-
-
-    console.log(followers, following, publicRepos, location, profilePic);
+console.log(res);
+  console.log(
+    name, 
+    location, 
+    bio,
+    publicRepos, 
+    followers, 
+    following,  
+    profilePic, 
+    profileUrl, 
+    blogUrl);
   });
 
   let countStars = 0
@@ -80,15 +51,18 @@ let profilePic = res.data.avatar_url;
   .then(function (res) {
   
     let i = 0
-    while(i<res.data.length){
-        countStars = countStars+res.data[i++].stargazers_count
+    while(i < res.data.length){
+      
+      countStars = countStars + res.data[i++].stargazers_count
       
     }
+ 
     console.log(countStars);
-    // stargazers_count
+ 
   });
 
 })
 
 
 
+// google npm html to pdf
---
+++ psuedoCode.js
@@ -0,0 +1,39 @@
+//Pull It Together App
+
+//A command line app that generates a PDF populated with 
+
+//GitHub API
+
+// the baground color for the pdf should match the user specified color
+
+//DONE create inquirer question to get username, favorite color, 
+//DONE run the api call
+//DONE when I get the response save the required info to variables
+
+//create a template literal for an HTML page that plugs in all the variables from the API call
+//linke the template literal to the index.js populate it with the appropriate variables
+//use npm html to pdf to and or fs write file to do the same thing a pdf from the template literal
+//write those variables to a pdf file with the favorite color as the background
+
+
+
+// DONE GitHub repository with a unique name and a README describing project.
+
+
+// The application generates a PDF resume from the user provided GitHub profile.
+
+
+// DONE The generated resume includes a bio image from the user's GitHub profile.
+
+
+// DONE The generated resume includes the user's location 
+// DONE and a link to their GitHub profile.
+
+
+// The background color of the generated PDF matches the color that the user provides.
+// https://api.github.com/users/AdenasGittinIt/repos - loop through this api and get the sumation of all stargazer counts
+
+//User location via Google Maps - GET CLARIFICATION ON THIS!!
+
+//I'll have to append the href to buttons on the HTML
+//I'll need to confirm that that is working.
